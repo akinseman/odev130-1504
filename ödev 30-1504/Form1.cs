8403cd5de60fd420bd3d3a9afe2d3c6485a7b56f
akinseman <145451296+akinseman@users.noreply.github.com>
2023-11-26 15:45:37 +0300

    Add files via upload

---
+++ ödev 30-1504/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ödev_30_1504
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = 0;
+            do
+            {
+               richTextBox1.Text=richTextBox1.Text+sayi.ToString();
+                sayi++;
+                while (sayi > 30) ;
+            } 
+        }
+    }
+}
